14565e32f68c83936c4b95ce5a6bd69f354bbac7
KJ <irateboolean@gmx.com>
2021-08-31 00:46:02 -0400

    added user model to admin

--- stones/sarasstones/admin.py
+++ stones/sarasstones/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Color, Mineral, Cut, Stone
+from .models import Color, Mineral, Cut, Stone, User
 # Register your models here.
 admin.site.register(Color)
 admin.site.register(Mineral)
 admin.site.register(Cut)
 admin.site.register(Stone)
+admin.site.register(User)
 

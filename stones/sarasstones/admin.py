986a7db5482af8fc920d0242eb3cdc498664c74a
KJ <irateboolean@gmx.com>
2021-08-31 10:50:14 -0400

    added Borrow model to admin

--- stones/sarasstones/admin.py
+++ stones/sarasstones/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Color, Mineral, Cut, Stone, User
+from .models import Borrow, Color, Mineral, Cut, Stone, User
 # Register your models here.
 admin.site.register(Color)
 admin.site.register(Mineral)
 admin.site.register(Cut)
 admin.site.register(Stone)
 admin.site.register(User)
+admin.site.register(Borrow)
 

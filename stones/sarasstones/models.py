dd3e26d88aa38bd7b9862443df6f06c2b72a7a7c
KJ <irateboolean@gmx.com>
2021-08-31 13:24:41 -0400

    added checkout user to stone

--- stones/sarasstones/models.py
+++ stones/sarasstones/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+
+
 # Create your models here.
 
 class Color(models.Model):
@@ -8,13 +10,15 @@ class Color(models.Model):
     def __str__(self):
         return self.name
 
+
 class Mineral(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    brief = models.CharField(max_length=200, null=True)
-    properties = models.CharField(max_length=2000, null=True)
+    brief = models.CharField(max_length=200, null=True, blank=True)
+    properties = models.CharField(max_length=2000, null=True, blank=True)
 
     def __str__(self):
         return self.name
+
 
 class Cut(models.Model):
     name = models.CharField(max_length=64, unique=True)
@@ -22,21 +26,24 @@ class Cut(models.Model):
     def __str__(self):
         return self.name
 
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Stone(models.Model):
     name = models.CharField(max_length=200)
     mineral = models.ForeignKey(Mineral, on_delete=models.PROTECT)
     cut = models.ForeignKey(Cut, on_delete=models.PROTECT)
     color = models.ForeignKey(Color, on_delete=models.PROTECT)
-    notes = models.CharField(max_length=2000, null=True)
-    img_path = models.CharField(max_length=100, unique=True, null=True)
+    notes = models.CharField(max_length=2000, null=True, blank=True)
+    img_path = models.CharField(max_length=100, unique=True, null=True, blank=True)
     acquire_date = models.DateTimeField()
     checked_out = models.BooleanField()
-
-    def __str__(self):
-        return self.name
-
-class User(models.Model):
-    name = models.CharField(max_length=100)
+    current_borrower = models.ForeignKey(User, on_delete=models.PROTECT, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -47,3 +54,18 @@ class Borrow(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     check_out = models.DateTimeField()
     check_in = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        desc = ''
+        if not self.check_in:
+            desc += 'OUT '
+        else:
+            desc += 'RETURNED '
+        desc += f'{self.user.name} : '
+        desc += f'{self.stone.name} \n'
+        if not self.check_in:
+            desc += f'Out for  {timezone.now() - self.check_out} \n'
+            desc += f'Checked out {self.check_out.date()}'
+        else:
+            desc += f'Out for {self.check_in - self.check_out} \n'
+        return desc

4bf891ebbac64f9f4ce5b1b2b597d33607091bad
KJ <irateboolean@gmx.com>
2021-08-31 20:24:15 -0400

    addeed imagepath to admin

--- stones/sarasstones/admin.py
+++ stones/sarasstones/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Borrow, Color, Mineral, Cut, Stone, User
+from .models import Borrow, Color, ImagePath, Mineral, Cut, Stone, User
 # Register your models here.
 admin.site.register(Color)
 admin.site.register(Mineral)
@@ -7,4 +7,4 @@ admin.site.register(Cut)
 admin.site.register(Stone)
 admin.site.register(User)
 admin.site.register(Borrow)
-
+admin.site.register(ImagePath)

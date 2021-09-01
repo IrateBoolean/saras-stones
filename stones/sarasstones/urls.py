f1de3a0f92883abc5cafc339fb80700adca38295
KJ <irateboolean@gmx.com>
2021-08-31 20:27:07 -0400

    added static path

--- stones/sarasstones/urls.py
+++ stones/sarasstones/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:mineral_id>/', views.detail, name='detail'),
     path('<int:mineral_id>/checkout/', views.checkout, name='checkout'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
